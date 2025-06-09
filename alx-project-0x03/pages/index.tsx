b517e835e7915f757ca712a49a58dbdce90ad901
DevBuike <142910858+DevBuike@users.noreply.github.com>
2025-06-09 10:31:21 +0000

    Move interfaces to the right directory

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -2,10 +2,7 @@ import Button from "@/components/common/Button";
 import { useRouter } from "next/router";
 import Header from "@/components/layouts/Header";
 import Footer from "@/components/layouts/Footer";
-
-interface PageRouteProps {
-  pageRoute: string
-}
+import { PageRouteProps } from "@/interface";
 
 export default function Home() {
   const router = useRouter();
